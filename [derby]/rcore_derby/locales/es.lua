6fddd44e708e9c3c4c47b98456d0cc49cd564c70
EmiN364 <68005487+EmiN364@users.noreply.github.com>
2021-06-17 14:24:28 -0300

    Create es.lua

---
+++ [derby]/rcore_derby/locales/es.lua
@@ -0,0 +1,14 @@
+Locales = {
+    ["points"] = "puntos",
+    ["start"] = '~r~YA!',
+
+    ["1_place"] = "~y~Primer lugar",
+    ["2_place"] = '~c~Segundo lugar',
+    ["3_place"] = '~o~Tercer lugar',
+
+    ["other_position"] = '~n~~w~%s Terminó en %i posición con %i puntos',
+    ["others"] = '~g~El resto',
+    -- server
+    ["join"] =  "El Jugador (%s) se ha unido a la arena (%s) Espacios restantes: %i/%i",
+    ["leave"] =  "El Jugador (%s) se fue de la arena (%s) Espacios restantes: %i/%i",
+}
